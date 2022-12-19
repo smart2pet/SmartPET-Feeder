7768e4e79de7c7bd73f533dead0fd8decaac2ac9
Ryan Zhang <smart_pet@126.com>
2022-12-19 12:19:49 +0800

    Add some content to README.md and uploaded train tool.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SmartPET feeder
+# SmartPET Feeder
 
 The open-source, easy-to-use feeder
 
@@ -11,7 +11,7 @@ The open-source, easy-to-use feeder
 + A outer shell (recommanded because the pet maybe break the machine.)
 #### For Espressif ESP32:
 + A Expressif ESP32 module (of course).
-#### For other hardware:
+#### For Other Hardware:
 + A stepper motor that is fit the size of moter in 3D model and a A4988 stepper motor driver.
 + You need 3D-print or some way to make the feeder hardware (the .obj 3D file is in `hardware` directory).
 + Some wires and power supply.
@@ -22,9 +22,27 @@ The open-source, easy-to-use feeder
 + Please use PlatformIO to open the project and just upload it.
 
 ### Usage
-#### Quick start.
-+ To use, run the start.sh file in the desktop and it will automatic run. About the model, you can convert it to hdf5 abd fine-tune it and it can fit your motor. I'll upload the script to the github later.
+#### Quick Start
++ To use, run the start.sh file in the desktop and it will automatic run. 
 
+#### Model Training
++ You may train your own model because the motor was diffrenet. Before training, please look at the comments in `other/train.py` and follow the instruction of the comment. And you need to run `python other/train.py`.
++ You must convert .h5 hdf5 format to the .tflite tensorflow lite format and replace `raspberry_pi/src/model.tflite` to your model.
+
+#### ESP32 Use
++ You can use it by write s{your rounds}; to the serial.
+
+#### Web API Use
++ Feed: POST /api/food JSON input: {"weight": $your feeding weight}
++ Add plan: POST /api/plan JSON input: {
+                                       "time_h": $hour,
+                                       "time_m": $minutes,
+                                       "weight": $weight
+                                       }
++ Delete plan: DELETE /api/plan JSON input:{
+                                           "time_h": $hour,
+                                           "time_m": $minutes,
+                                           }
 ## Contributing:
 
 Open a pull request is welcome for us. But if you want to make bug fixes or major changes such as a new function, etc., please create an issue first.
---
+++ other/train.py
@@ -0,0 +1,115 @@
+import os
+import sys
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
+from tensorflow import keras
+from tensorflow.keras import layers
+
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.3  # type: ignore
+tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config))
+
+train_x = [
+    [50],
+    [100],
+    [150],
+    [175],
+    [200],
+    [250],
+    [300],
+    [75],
+    [125],
+    [225],
+    [275],
+    [25],
+    [35],
+    [15],
+    [5],
+]
+# Bring your data here. train_y is the weight of food put out when turn the round in train_x, a data in train_y matches a data in train_x. Don't change train_x, 
+# you need to do these test and bring the weight of food put out in train_y (the unit is gram).
+train_y = [
+    [30],
+    [50],
+    [65],
+    [75],
+    [90],
+    [105],
+    [125],
+    [45],
+    [60],
+    [90],
+    [110],
+    [15],
+    [25],
+    [11],
+    [5],
+]
+train_x = np.array(train_x)
+print(train_x)
+train_y = np.array(train_y)
+test_x = [140, 240]
+test_y = [61, 89] # bring your result here.
+
+
+def build_model() -> tf.keras.Sequential:
+    """Builds a model.
+
+    Args:
+      None
+
+    Returns:
+      A compiled Keras model.
+    """
+    model = keras.Sequential(
+        [
+            layers.Dense(54, activation="relu"),
+            layers.Dense(32, activation="relu"),
+            layers.Dense(1),
+        ]
+    )
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
+    model.compile(loss="mse", optimizer="adam", metrics=["mae", "mse"])
+    return model  # type: ignore
+
+
+model = build_model()
+
+class PrintDot(keras.callbacks.Callback):
+    def on_epoch_end(self: object, epoch: int, logs: dict) -> None:
+        """
+        This is a multi-line Google style docstring.
+
+        Args:
+          self: The object.
+          epoch: The epoch.
+          logs: The logs.
+
+        Returns:
+          None.
+        """
+        if epoch % 100 == 0:
+            print("")
+        print(".", end="")
+
+
+EPOCHS = 1000000
+early_stop = keras.callbacks.EarlyStopping(
+    monitor="val_loss", patience=100, restore_best_weights=True
+)
+history = model.fit(
+    train_y,
+    train_x,
+    epochs=EPOCHS,
+    validation_split=0.3,
+    verbose=0,  # type: ignore
+    callbacks=[early_stop, PrintDot()],
+)
+model.save("./model.h5")
+test_predictions = model.predict([[60]])
+print(test_predictions)
